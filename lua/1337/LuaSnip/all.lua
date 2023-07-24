295aee7e0e6b303b30b875de01a3a92609c328ad
Yasmercy <ianchensoftware@gmail.com>
2023-07-23 20:33:08 -0700

    lsp-zero

--- lua/1337/LuaSnip/all.lua
+++ lua/1337/LuaSnip/all.lua
@@ -9,9 +9,21 @@ local d = ls.dynamic_node
 local fmt = require("luasnip.extras.fmt").fmt
 local fmta = require("luasnip.extras.fmt").fmta
 
+local get_visual = function(args, parent)
+  if (#parent.snippet.env.LS_SELECT_RAW > 0) then
+    return sn(nil, i(1, parent.snippet.env.LS_SELECT_RAW))
+  else  -- If LS_SELECT_RAW is empty, return a blank insert node
+    return sn(nil, i(1))
+  end
+end
+
 return {
     s(
         {trig = ";name", snippetType="autosnippet"},
         { t("Ian Chen") }
-    )
+    ),
+    s(
+        {trig = "str"},
+        fmt("\"<>\"", {d(1, get_visual)}, {delimiters="<>"})
+    ),
 }
--- lua/1337/init.lua
+++ lua/1337/init.lua
@@ -20,3 +20,31 @@ require("luasnip").config.set_config({ -- Setting LuaSnip config
     -- Use Tab (or some other key if you prefer) to trigger visual selection
     store_selection_keys = "<Tab>",
 })
+
+-- lsp
+local lsp = require('lsp-zero').preset({})
+lsp.on_attach(function(client, bufnr)
+    lsp.default_keymaps({buffer = bufnr})
+end)
+
+-- (Optional) Configure lua language server for neovim
+require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+require('lspconfig').pylsp.setup({})
+lsp.setup()
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  }
+})
